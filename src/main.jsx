223a95873f1ebcd4b663d6aaaa31af166cf55753
BurakKurt <krtburak@outlook.com>
2023-07-18 11:25:29 +0300

    feature(build): The project skeleton was created and related technologies were added.
    Redux Toolkit, React Helmet Async, React Icons, React Router Dom, TailwindCss

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.Fragment>
+    <p className="text-red-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</p>
+  </React.Fragment>,
+)
