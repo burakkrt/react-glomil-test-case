99b303656621e02fa92577e1563774d900f95f04
BurakKurt <krtburak@outlook.com>
2023-07-18 12:06:34 +0300

    feature(helmetAsync): provider layour and customHelmet component created

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import HelmetProvider, {CustomHelmet} from './helmet/HelmetProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.Fragment>
-    <p className="text-red-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</p>
-  </React.Fragment>,
+  <React.StrictMode>
+    <HelmetProvider>
+      <CustomHelmet/>
+      <p className="text-red-600">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</p>
+    </HelmetProvider>
+  </React.StrictMode>,
 )
