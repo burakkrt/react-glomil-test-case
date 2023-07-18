82990d5bddd40c6fa2bd404b9a41d105f72278e0
BurakKurt <krtburak@outlook.com>
2023-07-18 13:06:52 +0300

    feature(header&footer) component created

---
+++ src/pages/footer/index.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+
+  return (
+    <div className="bg-slate-700 text-slate-50 text-center h-20">
+      Bu alan footer içindir.
+    </div>
+  );
+}
---
+++ src/pages/navigation/index.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+
+  return (
+    <div className="h-20 bg-red-200 text-center">
+      Bu alan header içindir
+    </div>
+  );
+}
