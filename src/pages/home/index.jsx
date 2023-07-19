b604267a16ae159d7768e38235fe21cd7d88cab5
BurakKurt <krtburak@outlook.com>
2023-07-19 15:33:19 +0300

    Portal Live Product 1 created

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import WelcomePortal from "./WelcomePortal.jsx";
+import PortalLiveProducts from "./portal-live-products/PortalLiveProducts.jsx";
 
 export default function HomePageLayout() {
 
   return (
     <div>
       <WelcomePortal/>
+      <PortalLiveProducts/>
     </div>
   );
 }
