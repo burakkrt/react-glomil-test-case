cb29b04e248ee703d7d5604218b4fbe9058d7a4b
BurakKurt <krtburak@outlook.com>
2023-07-20 10:42:13 +0300

    welcome to portal responsive desing complated

--- src/pages/home/welcome-portal/WelcomePortal.jsx
+++ src/pages/home/welcome-portal/WelcomePortal.jsx
@@ -4,13 +4,16 @@ export default function WelcomePortal() {
 
   return (
     <div className="py-14" style={{backgroundColor: "#212846"}}>
-      <div className="container mx-auto flex flex-row items-center justify-evenly">
-        <div className="text-slate-50">
-          <span className="text-5xl font-bold">Portala Hoşgeldiniz</span>
-          <p className="w-96 text-lg text-slate-300 mt-8">Basit, sezgisel ve güzel deneyimler
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-evenly gap-y-14 lg:gap-y-14">
+        <div className="text-slate-50 text-center md:text-start">
+          <span className="text-3xl lg:text-5xl font-bold">Portala Hoşgeldiniz</span>
+          <p className="w-full sm:w-2/3 md:w-full lg:w-96 mx-auto md:mx-0  text-lg text-slate-300 mt-8">Basit, sezgisel
+            ve güzel
+            deneyimler
             oluşturmak için Portal'ın evrensel yazılım dilini kullanın.</p>
           <button
-            className="text-lg bg-blue-700 text-slate-200 py-2 px-4 mt-4 rounded hover:text-slate-50 duration-150">Hadi
+            className="text-md bg-blue-700 text-slate-200 py-2 px-4 mt-4 rounded hover:bg-blue-800 duration-150"
+            onClick={() => alert("Button Passive")}>Hadi
             Başlayalım
           </button>
         </div>
