fe5f3a7330b906987fce8568cd75a7a9f2398c93
burakkurt <krtburak@outlook.com>
2023-07-26 11:26:37 +0300

    feature(404ErrorPage): 404 error page style, responsive and functions completed

--- src/components/globalIcons.jsx
+++ src/components/globalIcons.jsx
@@ -1,16 +1,17 @@
-export {ImStatsDots as Icon01} from "react-icons/im";
-export {BsDisplay as Icon02} from "react-icons/bs";
-export {MdGTranslate as Icon03} from "react-icons/md";
-export {CgMenuGridR as Icon04} from "react-icons/cg";
-export {IoTextSharp as Icon05} from "react-icons/io5";
-export {AiOutlineArrowRight as Icon06} from 'react-icons/ai';
-export {FcApproval as Icon07} from "react-icons/fc";
-export {AiOutlineHome as Icon08} from "react-icons/ai";
-export {AiOutlineSearch as Icon09} from "react-icons/ai";
-export {IoIosNotifications as Icon10} from "react-icons/io"
-export {BsQuestionCircleFill as Icon11} from "react-icons/bs"
-export {IoIosSettings as Icon12} from "react-icons/io"
-export {BiSolidUser as Icon13} from "react-icons/bi"
+export { ImStatsDots as Icon01 } from "react-icons/im";
+export { BsDisplay as Icon02 } from "react-icons/bs";
+export { MdGTranslate as Icon03 } from "react-icons/md";
+export { CgMenuGridR as Icon04 } from "react-icons/cg";
+export { IoTextSharp as Icon05 } from "react-icons/io5";
+export { AiOutlineArrowRight as Icon06 } from "react-icons/ai";
+export { FcApproval as Icon07 } from "react-icons/fc";
+export { AiOutlineHome as Icon08 } from "react-icons/ai";
+export { AiOutlineSearch as Icon09 } from "react-icons/ai";
+export { IoIosNotifications as Icon10 } from "react-icons/io";
+export { BsQuestionCircleFill as Icon11 } from "react-icons/bs";
+export { IoIosSettings as Icon12 } from "react-icons/io";
+export { BiSolidUser as Icon13 } from "react-icons/bi";
+export { BsFillArrowLeftCircleFill as Icon14 } from "react-icons/bs";
 
 
 //example : ImTicket : icon01
