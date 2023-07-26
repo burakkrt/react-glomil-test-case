a3814a339faf08fcf7ddee44331f22efcc4fa2b5
burakkurt <krtburak@outlook.com>
2023-07-26 21:08:33 +0300

    feature(searchbar and router link): searchbar all function added and router new title link added

--- src/pages/navigation/right-infos/Search.jsx
+++ src/pages/navigation/right-infos/Search.jsx
@@ -1,12 +1,65 @@
-import {Icon09} from "../../../components/globalIcons.jsx"
+import { Icon09 } from "../../../components/globalIcons.jsx";
+import { router } from "../../../router/router.jsx";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+let arr = [];
+const search = (router) => {
+
+  router.map((route) => {
+    if (route.name) {
+      arr.push({ name: route.name, link: route.link });
+    }
+    if (route.children) {
+      search(route.children);
+    }
+  });
+  return arr;
+};
 
 export default function Search() {
+  arr = [];
+  // eslint-disable-next-line no-unused-vars
+  const [searchPages, setSearchPages] = useState(search(router));
+  const [searchInput, setSearchInput] = useState("");
+  const navigate = useNavigate();
+
+  function routerHandler(pageLink) {
+    navigate(pageLink);
+    setSearchInput("");
+    const searchResults = document.querySelector("#searchResults");
+    searchResults.classList.replace("visible", "invisible");
+  }
+
+  function visibleSearch() {
+    const searchResults = document.querySelector("#searchResults");
+    searchResults.classList.replace("invisible", "visible");
+  }
+
+  function inVisibleSearch() {
+    const searchResults = document.querySelector("#searchResults");
+    setTimeout(() => {
+      searchResults.classList.replace("visible", "invisible");
+    }, 300);
+  }
 
   return (
     <div className="relative">
-      <input className="py-1 rounded-lg border border-2 ps-12" type="text" placeholder="Search..."
-             style={{minWidth: "280px"}}/>
-      <Icon09 className="absolute top-1/2 start-3 -translate-y-1/2 text-2xl text-slate-500"/>
+      <div className="relative">
+        <input className="py-1 rounded-lg border-2 ps-12" onFocus={visibleSearch} onBlur={inVisibleSearch} type="text" placeholder="Search..."
+               style={{ minWidth: "280px" }} value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+        <Icon09 className="absolute top-1/2 start-3 -translate-y-1/2 text-2xl text-slate-500" />
+      </div>
+      <div className="invisible w-full absolute z-40 overflow-auto bg-slate-50 shadow-lg border-2 rounded mt-1 p-2" id="searchResults" style={{ maxHeight: "252px" }}>
+        {
+          searchPages.map((page) => {
+            if (page.name.toLowerCase()
+              .includes(searchInput.toLowerCase())) {
+              return <button key={page.name} className="w-full z-50 text-start text-lg text-slate-500 hover:bg-slate-200 py-1 px-2" onClick={() => routerHandler(page.link)}>{page.name}</button>;
+            }
+          })
+        }
+      </div>
     </div>
   );
 }
