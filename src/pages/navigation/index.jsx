9d73cba16a3d5dda47d2ef766e4451c4871cf650
burakkurt <krtburak@outlook.com>
2023-07-27 14:46:11 +0300

    feature(header-position-fixed): mobile design and function completed finish

--- src/pages/navigation/index.jsx
+++ src/pages/navigation/index.jsx
@@ -7,15 +7,36 @@ import Profile from "./Profile.jsx";
 import { Icon15, Icon16 } from "../../components/globalIcons.jsx";
 import { useSelector, useDispatch } from "react-redux";
 import { toogleResNavigation } from "../../stores/resNavigation.jsx";
+import { useEffect } from "react";
 
 export default function Header() {
 
   const { visible } = useSelector(state => state.resNavigation);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    const headerDOM = document.querySelector("header");
+    let list = ["fixed", "shadow-lg"];
+
+    window.addEventListener("scroll", () => {
+      if (scrollY >= headerDOM.clientHeight) {
+        if (headerDOM.className.includes("relative")) {
+          headerDOM.classList.remove("relative");
+          headerDOM.classList.add(...list);
+        }
+      }
+      if (scrollY < headerDOM.clientHeight) {
+        if (headerDOM.className.includes("fixed")) {
+          headerDOM.classList.remove(...list);
+          headerDOM.classList.add("relative");
+        }
+      }
+    });
+  }, []);
+
 
   return (
-    <header className="relative xl:flex px-3 xl:px-10 xl:gap-x-20">
+    <header className="w-full top-0 relative bg-slate-50 xl:flex px-3 xl:px-10 xl:gap-x-20 xl:my-0">
       <div className="w-full xl:w-fit flex flex-row items-center justify-between">
         <Logo />
         {visible ?
@@ -29,7 +50,7 @@ export default function Header() {
         <div className="sm:order-0 mx-auto sm:mx-0 lg:me-5">
           <Search />
         </div>
-        <div className="flex flex-row items-center gap-x-3 sm:mb-0 px-1 mx-auto sm:mx-0">
+        <div className="flex flex-row flex-wrap items-center gap-x-3 gap-y-2 sm:mb-0 px-1 mx-auto sm:mx-0">
           <NavSettings />
           <Profile />
         </div>
